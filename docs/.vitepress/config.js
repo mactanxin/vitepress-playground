a862ba2635e3472057f59978fbd14f6392a171cb
Xin Tan <mactanxin@gmail.com>
2022-08-31 09:20:01 +0800

    feat(config): 新增动态sidebar

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,11 @@ export default {
   themeConfig: {
     siteTitle: 'Xin Vitepress',
     nav: nav(),
-    sidebar: sideBarnav()
+    sidebar: sideBarnav(),
+    /* sidebar: { */
+    /*   '/guide/': sidebarGuide(), */
+    /*   '/config/': sidebarConfig() */
+    /* }, */
   }
 }
 // 顶部导航栏
@@ -24,8 +28,23 @@ function sideBarnav () {
       items: [
         { text: 'Home sweet home', link: '/'},
         { text: 'Getting Started', link: '/getting-started'},
+        { text: 'State sharing', link: '/getting-started'},
         { text: 'Foo Index', link: '/foo/index'},
         { text: 'Foo 123', link: '/foo/123'},
+      ]
+    }
+  ]
+}
+
+function sidebarConfig() {
+  return [
+    {
+      text: 'Config',
+      items: [
+        { text: 'Introduction', link: '/config/introduction' },
+        { text: 'App Configs', link: '/config/app-configs' },
+        { text: 'Theme Configs', link: '/config/theme-configs' },
+        { text: 'Frontmatter Configs', link: '/config/frontmatter-configs' }
       ]
     }
   ]
